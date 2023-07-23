2a9207a53ca12add3c76b6a45d11bf2c676271ff
kevinrockstar25@gmail.com <kevinrockstar25@gmail.com>
2023-07-23 08:33:59 +0530

    github

---
+++ passcrack.py
@@ -0,0 +1,9 @@
+from random import*
+user_pass=("enter your password")
+password = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+gauss = 
+while(gauss)!=user_pass):
+  gauss = 
+  for letter in range(len(user_pass)):
+    gauss_letter = password[ranrange(0.25)]
+    gauss = str()
